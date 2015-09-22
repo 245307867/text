79206a310e751d4149342a9d1e39b0ac1f3090fd
dongsen <245307867@qq.com>
2015-09-22 11:40:06 +0800

    测试一次

---
+++ text/text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by maple on 15/9/22.
+//  Copyright (c) 2015年 oschina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by maple on 15/9/22.
+//  Copyright (c) 2015年 oschina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
